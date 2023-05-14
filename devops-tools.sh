8e992e3ba9ec3031cafa72bcc2b6ff67d902abfe
Devilicht <oblivionccyber@gmail.com>
2023-05-14 15:31:43 -0400

    feat Error Handling

--- devops-tools.sh
+++ devops-tools.sh
@@ -1,31 +1,63 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
 function install_git {
     echo "Installing Git..."
-    apt-get update && apt-get install -y git
-    echo "Git installed successfully!"
+    apt-get -qq update && apt-get install -qqy git
+    if [ "$?" != "0" ]; then
+        echo "Error installing Git"
+        exit 1
+    fi  
+        echo "Git installed successfully!"
 }
 
 function install_docker {
     echo "Installing Docker..."
-    apt-get update && apt-get install -y docker.io && apt-get install compose
+    apt-get -qq update && apt-get install -qqy docker.io && apt-get install -qq docker-compose
+    if [ "$?" != "0" ]; then
+        echo "Error installing Docker"
+        exit 1
+    fi
     usermod -aG docker $USER
     echo "Docker installed successfully!"
 }
 
 function install_ansible {
     echo "Installing Ansible..."
-    apt-get update && apt-get install -y ansible
+    apt-get -qq update && apt-get install -yqq ansible
+    if [ "$?" != "0" ]; then
+        echo "Error installing Ansible"
+        exit 1
+    fi
     echo "Ansible installed successfully!"
 }
 
 function install_jenkins {
     echo "Installing Jenkins..."
-    wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
-    echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
-    apt-get update && apt-get install -y jenkins
+    curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+    echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    if ! sudo apt-get -qq update; then
+        echo "Error updating package index. Aborting installation."
+        return 1
+    fi
+    if ! sudo apt-get install -yqq fontconfig openjdk-11-jre; then
+        echo "Error installing required packages. Aborting installation."
+        return 1
+    fi
+    if ! sudo apt-get install -yqq jenkins; then
+        echo "Error installing Jenkins. Aborting installation."
+        return 1
+    fi
     echo "Jenkins installed successfully!"
 }
+
 
 function install_all {
     install_git
@@ -44,19 +76,19 @@ function help {
 }
 
 case $1 in
-    "install git")
+    "install-git")
     install_git
     ;;
-    "install docker")
+    "install-docker")
     install_docker
     ;;
-    "install ansible")
+    "install-ansible")
     install_ansible
     ;;
-    "install jenkins")
+    "install-jenkins")
     install_jenkins
-    ;;
-    "install all")
+    ;;  
+    "install-all")
     install_all
     ;;
     *)
